1705859edb63661e2c24afa849974566d6b0f23c
Matthew Lewis <90684339+MattyJayLewis@users.noreply.github.com>
2021-09-20 13:35:52 +0100

    Update Chapter2.py

--- Chapter2.py
+++ Chapter2.py
@@ -77,3 +77,12 @@ print("These numbers divided are " + str(var_a + var_b)
 
 
 print("\nThat's all, folks!")
+      
+## 2.6.1.11 ##
+      
+hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+time_hour = (hour + dura//60 + (mins+ dura%60)//60)%24
+time_min = (mins+ dura%60)%60
+print("It will end at " + str(time_hour) + ":" + str(time_min))      
