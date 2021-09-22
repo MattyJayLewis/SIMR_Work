55b66a544e00e2ffb3506b4395a92840702e0b87
Matthew Lewis <90684339+MattyJayLewis@users.noreply.github.com>
2021-09-22 11:47:21 +0100

    Update Chapter3.py

--- Chapter3.py
+++ Chapter3.py
@@ -130,3 +130,15 @@ for letter in user_word:
       
 ## 3.2.1.15 ##
 
+n = int(input("Enter a strictly positive integer: "))
+steps = 0
+while n != 1:
+    if n % 2 == 0:
+        n = n / 2
+    elif n % 2 == 1: 
+        n = (3*n) + 1
+    steps += 1
+    print(int(n))
+
+print (steps)
+
