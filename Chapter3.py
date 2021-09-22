e2192831ad46cd125ad128a196898bad5ff5ca53
Matthew Lewis <90684339+MattyJayLewis@users.noreply.github.com>
2021-09-22 11:58:05 +0100

    Update Chapter3.py

--- Chapter3.py
+++ Chapter3.py
@@ -159,3 +159,41 @@ print(hat_list)
 
 ## 3.4.1.13 ##
 
+#step 1
+
+beatles = []
+
+print(beatles)
+
+# step 2
+
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+
+print("Step 2:", beatles)
+
+# step 3
+
+for i in range(2):
+    member = input("next members : ")
+    beatles.append(member)
+
+print("Step 3:", beatles)
+
+# step 4
+
+for i in range(2):
+    length = len(beatles)-1
+    del beatles [length]
+
+print("Step 4:", beatles)
+
+# step 5
+
+beatles.insert(0, "Ringo Starr")
+print("Step 5:", beatles)
+
+
+# testing list legth
+print("The Fab", len(beatles))
